92acfb004f44e25f71d8e9a0f79deeae9ef3adb4
Leon Masopust <leon.masopust@student.hpi.de>
2019-05-05 17:29:11 +0200

    TTTC now uses the new class slot

--- lua/classes/shared/hooks.lua
+++ lua/classes/shared/hooks.lua
@@ -346,7 +346,7 @@ hook.Add("PlayerPostThink", "TTTCSetWeaponKind", function(ply)
 	if GetConVar("tttc_traitorbuy"):GetBool() and not ply.TTTCKindSet then
 		for _, w in pairs(ply:GetWeapons()) do
 			if w:GetNWBool("TTTC_class_weapon") then
-				w.Kind = WEAPON_EXTRA
+				w.Kind = WEAPON_CLASS
 				w.Doublicated = true
 
 				ply.TTTCKindSet = true
