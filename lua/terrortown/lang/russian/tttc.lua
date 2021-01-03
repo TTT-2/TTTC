a8727a449213f76e6a7330be87a8da3b4730358f
Aram <dragolino6@mail.ru>
2021-01-03 18:18:01 +0300

    WIP: Localization changes and improvements

--- lua/terrortown/lang/russian/tttc.lua
+++ lua/terrortown/lang/russian/tttc.lua
@@ -1,10 +1,10 @@
 local L = LANG.GetLanguageTableReference("Русский")
 
-L["ttt2_tttc_abort_ability"] = "Отменить предпросмотр способностей"
+L["ttt2_tttc_abort_ability"] = "Отмена предпросмотра способности"
 L["ttt2_tttc_class_ability"] = "Способность класса"
-L["ttt2_tttc_drop_class"] = "Оставить класс"
+L["ttt2_tttc_drop_class"] = "Сброс класса"
 L["ttt2_tttc_class"] = "КЛАСС"
 L["ttt2_tttc_class_unknown"] = "НЕИЗВЕСТНО"
-L["ttt2_tttc_class_desc_not_provided"] = "Описание класса не предоставлено, перейдите на новый формат класса."
+L["ttt2_tttc_class_desc_not_provided"] = "Описание класса не предоставлено. Перейдите на новый формат."
 
---L["header_bindings_classes"] = "TTT2 Classes"
+L["header_bindings_classes"] = "Классы TTT2"
