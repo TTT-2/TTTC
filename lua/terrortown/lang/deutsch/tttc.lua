f26ed665297bc639b40eff4777eb8b03792744c6
Aram <dragolino6@mail.ru>
2021-01-03 18:11:25 +0300

    Update

--- lua/terrortown/lang/deutsch/tttc.lua
+++ lua/terrortown/lang/deutsch/tttc.lua
@@ -1,4 +1,4 @@
-local L = LANG.GetLanguageTableReference("Deutsch")
+local L = LANG.GetLanguageTableReference("deutsch")
 
 L["ttt2_tttc_abort_ability"] = "Abbruch Klassen Fähigkeit"
 L["ttt2_tttc_class_ability"] = "Klassen Fähigkeit"
@@ -6,3 +6,5 @@ L["ttt2_tttc_drop_class"] = "Klasse Verlieren"
 L["ttt2_tttc_class"] = "KLASSE"
 L["ttt2_tttc_class_unknown"] = "UNBEKANNT"
 L["ttt2_tttc_class_desc_not_provided"] = "Keine Klassenbeschreibung verfügbar, bitte wechsel zum neuen Klassensystem."
+
+L["header_bindings_classes"] = "TTT2 Klassen"
