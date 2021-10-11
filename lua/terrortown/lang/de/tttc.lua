606ae71f99620e4c702301cc2b2d1468c4123bec
NickCloud <nicolasbeiglboeck@gmx.at>
2021-10-11 15:36:22 +0200

    Fixed typo

--- lua/terrortown/lang/de/tttc.lua
+++ lua/terrortown/lang/de/tttc.lua
@@ -25,4 +25,4 @@ L["label_tttc_classes_popup"] = "Aktiviere Klasseninfo Popup"
 L["label_tttc_classes_teamsync"] = "Sollen Klassen zu Teammates synchronisiert werden?"
 L["label_tttc_classes_different"] = "Die obere Grenze für Klassen (0: unbegrenzt)"
 
-L["help_tttc_calsses_limited"] = "Wenn aktivert, dann versucht das Spiel jede Klasse nur einmal zuzuweisen. Der selbe Klassenpool wird jedoch erneut verwendet, wenn es mehr Spieler als Klassen gibt."
+L["help_tttc_classes_limited"] = "Wenn aktivert, dann versucht das Spiel jede Klasse nur einmal zuzuweisen. Der selbe Klassenpool wird jedoch erneut verwendet, wenn es mehr Spieler als Klassen gibt."
