66fd266f34953561a8f8519f6b77cf3dd998ba60
Sirzento <sirzento@gmx.de>
2021-11-10 22:10:07 +0100

    Added a class assignment possibility

--- lua/terrortown/lang/en/tttc.lua
+++ lua/terrortown/lang/en/tttc.lua
@@ -15,6 +15,7 @@ L["menu_tttc_description"] = "Settings for TTTC Classes"
 L["submenu_addons_tttc_title"] = "TTTC"
 
 L["label_tttc_class_enabled"] = "Enable class"
+L["label_tttc_classes_random"] = "Chance that classes are assigned at the start of a round"
 L["label_tttc_class_spawn_chance"] = "Chance for class to be added to class pool"
 L["label_tttc_classes_enable"] = "Enable TTTC classes?"
 L["label_tttc_classes_limited"] = "Limit classes to one player only?"
